dd9898e2bdb9fb984b62e60781a8a3d24b4d1af5
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2021-12-02 21:38:16 +0100

    partial 04

--- AoC2020/04/aoc04.php
+++ AoC2020/04/aoc04.php
@@ -5,57 +5,62 @@ function readMyFile($fname)
 
     $text = file_get_contents($fname);
 
-    return explode(PHP_EOL.PHP_EOL, $text);;
+    return explode(PHP_EOL . PHP_EOL, $text);;
 }
 
 function myTestOne()
 {
     $array = readMyFile('text.text');
-    $compare_array = ['ecl', 'pid', 'eyr', 'hcl',
-    'byr', 'iyr', 'hgt'];
+    $compare_array = [
+        'ecl', 'pid', 'eyr', 'hcl',
+        'byr', 'iyr', 'hgt'
+    ];
     $counter = 0;
-    foreach($array as $line) {
+    foreach ($array as $line) {
         $line = str_replace(PHP_EOL, ' ', $line);
         $exploded = explode(' ', $line);
         $fields = [];
-        foreach($exploded as $member) {
+        foreach ($exploded as $member) {
             $fields[] = explode(':', $member)[0];
         }
         if (empty(array_diff($compare_array, $fields))) $counter++;
     }
     return $counter;
 }
+
 function myTestTwo()
 {
     $array = readMyFile('text.text');
-    $divider = strlen($array[0]) - 2;
-    $product = 1;
-    for ($step = 1; $step <= 7; $step += 2) {
-        $slopeDistance = 0;
-        $treeHits = 0;
-        foreach ($array as $line) {
-            if ($line[$slopeDistance % $divider] === "#") {
-                $treeHits++;
-            }
-            $slopeDistance += $step;
+    $compare_array = [
+        'ecl', 'pid', 'eyr', 'hcl',
+        'byr', 'iyr', 'hgt'
+    ];
+    $counter = 0;
+    foreach ($array as $line) {
+        $line = str_replace(PHP_EOL, ' ', $line);
+        $exploded = explode(' ', $line);
+        $fields = [];
+        foreach ($exploded as $member) {
+            $keyvalye = explode(':', $member);
+            $key = $keyvalye[0];
+            $val = $keyvalye[1];
+            $fields[$key] = $val;
         }
+        var_dump($fields);
 
-        $product *= $treeHits;
+        $truefalse = (!(is_numeric($fields['eyr']) and ($fields['eyr']>=2020) and ($fields['eyr']<=2030)));
+        var_dump($truefalse);
+        echo "all " . ($truefalse) . PHP_EOL;
+        if (!empty(array_diff($compare_array, array_keys($fields)))) continue;
+        if (!(is_numeric($fields['byr']) and ($fields['byr']>=1920) and ($fields['byr']<=2002))) continue;
+        if (!(is_numeric($fields['iyr']) and ($fields['iyr']>=2010) and ($fields['iyr']<=2020))) continue;
+        if (!(is_numeric($fields['eyr']) and ($fields['eyr']>=2020) and ($fields['eyr']<=2030))) continue;
+
+
+        echo "temp count|" . $counter . PHP_EOL;
+        $counter++;
     }
-
-    $slopeDistance = 0;
-    $treeHits = 0;
-    for ($i = 0; $i < count($array); $i += 2) {
-        if ($array[$i][$slopeDistance % $divider] === "#") {
-            $treeHits++;
-        }
-        $slopeDistance++;
-    }
-
-    $product *= $treeHits;
-
-
-    return $product;
+    return $counter;
 }
 
 
@@ -67,9 +72,9 @@ $end = (float) array_sum(explode(' ', microtime()));
 echo PHP_EOL . "Test One COMPLETED in:" . sprintf("%.4f", ($end - $start)) . " seconds." . PHP_EOL;
 
 
-// $start = (float) array_sum(explode(' ', microtime()));
+$start = (float) array_sum(explode(' ', microtime()));
 
-// echo myTestTwo();
+echo myTestTwo();
 
-// $end = (float) array_sum(explode(' ', microtime()));
-// echo PHP_EOL . "Test Two COMPLETED in:" . sprintf("%.4f", ($end - $start)) . " seconds.";
+$end = (float) array_sum(explode(' ', microtime()));
+echo PHP_EOL . "Test Two COMPLETED in:" . sprintf("%.4f", ($end - $start)) . " seconds.";
