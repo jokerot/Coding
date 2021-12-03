9ca2b8d4f14741ec058c5ef9ce4ac76096539f1b
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2021-12-03 02:13:26 +0100

    2020 04 finish with dumps

--- AoC2020/04/aoc04.php
+++ AoC2020/04/aoc04.php
@@ -11,6 +11,7 @@ function readMyFile($fname)
 function myTestOne()
 {
     $array = readMyFile('text.text');
+    $passed = [];
     $compare_array = [
         'ecl', 'pid', 'eyr', 'hcl',
         'byr', 'iyr', 'hgt'
@@ -46,20 +47,90 @@ function myTestTwo()
             $val = $keyvalye[1];
             $fields[$key] = $val;
         }
-        var_dump($fields);
-
-        $truefalse = (!(is_numeric($fields['eyr']) and ($fields['eyr']>=2020) and ($fields['eyr']<=2030)));
-        var_dump($truefalse);
-        echo "all " . ($truefalse) . PHP_EOL;
-        if (!empty(array_diff($compare_array, array_keys($fields)))) continue;
-        if (!(is_numeric($fields['byr']) and ($fields['byr']>=1920) and ($fields['byr']<=2002))) continue;
-        if (!(is_numeric($fields['iyr']) and ($fields['iyr']>=2010) and ($fields['iyr']<=2020))) continue;
-        if (!(is_numeric($fields['eyr']) and ($fields['eyr']>=2020) and ($fields['eyr']<=2030))) continue;
 
 
-        echo "temp count|" . $counter . PHP_EOL;
+        // $truefalse = (!(is_numeric($fields['eyr']) and ($fields['eyr']>=2020) and ($fields['eyr']<=2030)));
+        // var_dump($truefalse);
+        // echo "all " . ($truefalse) . PHP_EOL;
+        if (!empty(array_diff($compare_array, array_keys($fields)))) {
+            continue;
+        }
+        if (!(is_numeric($fields['byr']) and ($fields['byr'] >= 1920) and ($fields['byr'] <= 2002))) {
+            continue;
+        }
+        if (!(is_numeric($fields['iyr']) and ($fields['iyr'] >= 2010) and ($fields['iyr'] <= 2020))) {
+            continue;
+        }
+        if (!(is_numeric($fields['eyr']) and ($fields['eyr'] >= 2020) and ($fields['eyr'] <= 2030))) {
+            continue;
+        }
+        // var_dump(substr($fields['hgt'], -2, 2));
+        // var_dump(substr($fields['hgt'], 0, strlen($fields['hgt']) - 2));
+        $height = substr($fields['hgt'], 0, strlen($fields['hgt']) - 2);
+        $measure = substr($fields['hgt'], -2, 2);
+        // echo "cm: ";
+        // var_dump((is_numeric($height) and (intval($height) >= 150) and (intval($height) <= 193)));
+        // echo "in: ";
+        // var_dump((is_numeric($height) and (intval($height) >= 59) and (intval($height) <= 76)));
+        switch ($measure) {
+            case "cm":
+                if (!(is_numeric($height) and (intval($height) >= 150) and (intval($height) <= 193))) {
+                    continue 2;
+                }
+                break;
+            case "in":
+                if (!(is_numeric($height) and (intval($height) >= 59) and (intval($height) <= 76))) {
+                    continue 2;
+                }
+                break;
+            default:
+                continue 2;
+        }
+        if (!preg_match("/#[0-9a-f]{6}/", $fields['hcl'])) {
+
+            continue;
+        }
+        if (!(in_array($fields['ecl'], ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]))) {
+            continue;
+        }
+        if (!preg_match("/^\d{9}$/", $fields['pid'])) {
+            continue;
+        }
+
+        $passed[] = $fields;
         $counter++;
     }
+
+    file_put_contents('dump.json', json_encode($passed));
+
+    $pid = array_column($passed, 'pid');
+    sort($pid);
+    file_put_contents('pid.json', json_encode($pid));
+
+    $byr = array_column($passed, 'byr');
+    sort($byr);
+    file_put_contents('byr.json', json_encode($byr));
+
+    $iyr = array_column($passed, 'iyr');
+    sort($iyr);
+    file_put_contents('iyr.json', json_encode($iyr));
+
+    $eyr = array_column($passed, 'eyr');
+    sort($eyr);
+    file_put_contents('eyr.json', json_encode($eyr));
+
+    $hgt = array_column($passed, 'hgt');
+    sort($hgt);
+    file_put_contents('hgt.json', json_encode($hgt));
+
+    $hcl = array_column($passed, 'hcl');
+    sort($hcl);
+    file_put_contents('hcl.json', json_encode($hcl));
+
+    $ecl = array_column($passed, 'ecl');
+    sort($ecl);
+    file_put_contents('ecl.json', json_encode($ecl));
+
     return $counter;
 }
 
