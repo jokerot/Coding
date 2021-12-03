27e64e483c2ea6ebb6a32fdd50b12d4e4f4a0bfa
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2021-12-03 08:10:35 +0100

    2021 03 done

--- AoC2021/03/aoc03.php
+++ AoC2021/03/aoc03.php
@@ -31,30 +31,25 @@ function myTestOne()
     $epsilonRate = implode("", array_map(function ($x) use ($valuesCount) {
         return ($x > $valuesCount / 2) ? 0 : 1;
     }, $sums));
-    // echo 'gama array:  ';
-    // var_dump($gammaRate);
+
 
     return bindec($gammaRate) * bindec($epsilonRate);
 }
+
+
 function myTestTwo()
 {
     $values = readMyFile('text.text');
     $workingArray = $values;
     file_put_contents('values.json', json_encode($values));
-    $valuesCount = count($values);
-    echo "vCount= " . $valuesCount . PHP_EOL;
-    $gammaRate = [];
-    $epsilonRate = [];
-    $sums = array_fill(0, count($values[0]), 0);
+
     $foundIt = false;
     $k = 0;
     while (!$foundIt) {
         $sum = 0;
         foreach ($workingArray as $line) {
-            echo "line= " . $line[$k] . PHP_EOL;
             $sum += $line[$k];
         }
-        // echo "sum= " . $sum . PHP_EOL;
         $matchingValue = ($sum >= count($workingArray) / 2) ? 1 : 0;
         $workingArray = array_filter(
             $workingArray,
@@ -62,30 +57,23 @@ function myTestTwo()
                 return ($x[$k] == $matchingValue);
             }
         );
-        // echo "------matchingV-----------workingArr---------" . PHP_EOL;
-        // var_dump($matchingValue, $workingArray);
         $foundIt = (count($workingArray) === 1) ? true : false;
         $k++;
     }
-    // echo "!!!!!!!!!!!!!!!!!!!!!!!! oxy" . PHP_EOL;
-    // var_dump($workingArray);
     foreach ($workingArray as $flat) {
         $flatArray = $flat;
     }
-    var_dump($flatArray);
     $oxygen = implode("", $flatArray);
-    
-    
+
     $workingArray = $values;
     $foundIt = false;
     $k = 0;
     while (!$foundIt) {
         $sum = 0;
         foreach ($workingArray as $line) {
-            // echo "line= " . $line[$k] . PHP_EOL;
             $sum += $line[$k];
         }
-        // echo "sum= " . $sum . PHP_EOL;
+
         $matchingValue = ($sum >= count($workingArray) / 2) ? 0 : 1;
         $workingArray = array_filter(
             $workingArray,
@@ -93,21 +81,16 @@ function myTestTwo()
                 return ($x[$k] == $matchingValue);
             }
         );
-        // echo "------matchingV-----------workingArr---------" . PHP_EOL;
-        // var_dump($matchingValue, $workingArray);
+
         $foundIt = (count($workingArray) === 1) ? true : false;
         $k++;
     }
 
-    // echo "!!!!!!!!!!!!!!!!!!!!!!!! co2" . PHP_EOL;
-    // var_dump($workingArray);
-    // var_dump($workingArray);
     foreach ($workingArray as $flat) {
         $flatArray = $flat;
     }
     $co2 = implode("", $flatArray);
-    // echo "oxygen= " . $oxygen;
-    // echo "co2= " . $co2;
+
     return bindec($oxygen) * bindec($co2);
 }
 
