d5cc64f4c3afcc7b18aa55c88585c76c901327de
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2021-12-12 11:47:25 +0100

    aoc2021 12 more cleaning

--- AoC2021/12/aoc12.py
+++ AoC2021/12/aoc12.py
@@ -13,7 +13,7 @@ def create_data_structure(initial_data):
         if cave_name in result.keys():
             result[cave_name].append(conn_caves)
         else:
-             result[cave_name] = [conn_caves]
+            result[cave_name] = [conn_caves]
 
         if conn_caves in result.keys():
             result[conn_caves].append(cave_name)
@@ -21,11 +21,8 @@ def create_data_structure(initial_data):
             result[conn_caves] = [cave_name]
     return result
 
-
 caves = create_data_structure(ls)
-
 all_paths = []
-
 
 def walk_the_cave(current_path, current_cave):
     current_path.append(current_cave)
@@ -37,16 +34,16 @@ def walk_the_cave(current_path, current_cave):
         if i not in current_path or i.upper() == i:
             walk_the_cave(current_path.copy(), i)
         else:
-            continue    
+            continue
     return
 
 walk_the_cave([], "start")
 print("num of paths:", len(all_paths))
-all_paths2 = []
 
+all_paths2 = []
 def walk_the_cave2(current_path, current_cave, small_done):
     if current_cave.upper() != current_cave and current_cave != "start" and current_cave in current_path:
-        small_done = True 
+        small_done = True
 
     current_path.append(current_cave)
 
@@ -55,11 +52,11 @@ def walk_the_cave2(current_path, current_cave, small_done):
         return
 
     for i in caves[current_cave]:
-        if (i not in current_path or i.upper() == i or not small_done ) and i != "start"  :
+        if (i not in current_path or i.upper() == i or not small_done) and i != "start":
             walk_the_cave2(current_path.copy(), i, small_done)
         else:
-            continue    
+            continue
     return
 
-walk_the_cave2([], "start", False )
+walk_the_cave2([], "start", False)
 print("num of paths2:", len(all_paths2))
