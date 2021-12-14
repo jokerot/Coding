5ac7ae1537b1a0a92414e329b42b57fc90efbc1f
Zoran Alagjozovski (Joker) <71126675+jokerot@users.noreply.github.com>
2021-12-14 11:31:07 +0100

    format

--- AoC2021/14/aoc14.php
+++ AoC2021/14/aoc14.php
@@ -8,7 +8,6 @@ function readMyFile($fname)
 {
 
     $text = file($fname, FILE_IGNORE_NEW_LINES);
-
     $arr = [];
 
     foreach ($text as $line) {
@@ -27,29 +26,43 @@ function insert_elements($arr, $code)
         $newarr[] = $code[$arr[$i] . $arr[$i + 1]];
     }
     $newarr[] = end($arr);
-    return $newarr;
-}
 
-function insert_letter($arr, $code)
-{
+    return $newarr;
 }
 
 function step()
 {
     global $global_pair_counter, $global_polymer_l_count, $code;
     $new_pair_counter = $global_pair_counter;
-    $new_pair_counter = array_fill_keys (array_keys($new_pair_counter), 0);
+    $new_pair_counter = array_fill_keys(array_keys($new_pair_counter), 0);
     foreach ($global_pair_counter as $pair => $count) {
-        if ($count>0) {
-        $new_letter = $code[$pair];
-        $new_pair_1 = $pair[0] . $new_letter;
-        $new_pair_2 = $new_letter . $pair[1];
-        $new_pair_counter[$new_pair_1] += $count;
-        $new_pair_counter[$new_pair_2] += $count;
-        $global_polymer_l_count[$new_letter] += $count;
+        if ($count > 0) {
+            $new_letter = $code[$pair];
+            $new_pair_1 = $pair[0] . $new_letter;
+            $new_pair_2 = $new_letter . $pair[1];
+            $new_pair_counter[$new_pair_1] += $count;
+            $new_pair_counter[$new_pair_2] += $count;
+            $global_polymer_l_count[$new_letter] += $count;
         }
     }
+
     $global_pair_counter = $new_pair_counter;
+}
+
+function myTestOne()
+{
+    $polymer = "VFHKKOKKCPBONFHNPHPN";
+
+    $polymer = str_split($polymer);
+
+    $code = readMyFile('input.txt');
+    for ($j = 0; $j < 10; $j++) {
+        $polymer = insert_elements($polymer, $code);
+    }
+
+    $counts = array_count_values($polymer);
+
+    return max($counts) - min($counts);
 }
 
 function myTestTwo()
@@ -68,7 +81,7 @@ function myTestTwo()
         $global_polymer_l_count[$c] = 0;
     }
 
-    foreach(str_split($polymer) as $letter){
+    foreach (str_split($polymer) as $letter) {
         $global_polymer_l_count[$letter]++;
     }
 
@@ -82,24 +95,6 @@ function myTestTwo()
 
     return max($global_polymer_l_count) - min($global_polymer_l_count);
 }
-function myTestOne()
-{
-    $polymer = "VFHKKOKKCPBONFHNPHPN";
-
-    $polymer = str_split($polymer);
-
-    $code = readMyFile('input.txt');
-    for ($j = 0; $j < 10; $j++) {
-        $polymer = insert_elements($polymer, $code);
-    }
-
-    $counts = array_count_values($polymer);
-
-    return max($counts) - min($counts);
-}
-
-
-
 
 
 $start = (float) array_sum(explode(' ', microtime()));
